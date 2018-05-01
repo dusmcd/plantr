e6661ac6a0b9c3bb1f934c8569f6133430c87a56
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-05-01 16:13:16 -0500

    join tables

--- seed.js
+++ seed.js
@@ -1,31 +1,46 @@
 const { db, Gardener, Plot, Vegetable } = require('./model');
+const PlotVegetable = db.model('veg_plot');
 
 db
   .sync({ force: true })
   .then(() => {
-    return Vegetable.create({
-      name: 'Carrot',
-      color: 'Orange',
-    });
-  })
-  .then(veg => {
-    return Gardener.create({
-      name: 'Bob',
-      age: 35,
-      favoriteVegetableId: veg.id,
-    });
-  })
-  .then(gardener => {
-    return Plot.create({
-      size: 100,
-      shaded: true,
-      gardenerId: gardener.id,
-    });
-  })
-  .then(() => {
-    db.close();
+    makePromise()
+      .then(values => {
+        return PlotVegetable.create({
+          vegetableId: values[0].id,
+          plotId: values[1].id,
+        });
+      })
+      .then(() => {
+        db.close();
+      });
   })
   .catch(err => {
     console.error(err.message);
     db.close();
   });
+
+function makePromise() {
+  const vegetableP = Vegetable.create({
+    name: 'Tomato',
+    color: 'Red',
+  });
+
+  const plotP = vegetableP
+    .then(veg => {
+      return Gardener.create({
+        name: 'Rick',
+        age: 40,
+        favoriteVegetableId: veg.id,
+      });
+    })
+    .then(gardener => {
+      return Plot.create({
+        size: 400,
+        shaded: true,
+        gardenerId: gardener.id,
+      });
+    });
+
+  return Promise.all([vegetableP, plotP]);
+}
