391371cb712705f39fb336368422c71ecb52bd3d
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-05-02 18:57:54 -0500

    got it working

--- seed.js
+++ seed.js
@@ -4,13 +4,16 @@ const PlotVegetable = db.model('veg_plot');
 db
   .sync({ force: true })
   .then(() => {
-    return makePromise();
+    return makePromises();
   })
   .then(values => {
-    return PlotVegetable.create({
-      vegetableId: values[0].id,
-      plotId: values[1].id,
+    const plotVegArray = values[0].map((veg, i) => {
+      return PlotVegetable.create({
+        vegetableId: veg.id,
+        plotId: values[1][i].id,
+      });
     });
+    return Promise.all(plotVegArray);
   })
   .then(() => {
     db.close();
@@ -20,30 +23,38 @@ db
     db.close();
   });
 
-function makePromise() {
-  const vegetableP = Vegetable.create({
-    name: 'Tomato',
-    color: 'Red',
-  });
-
-  const plotP = vegetableP
-    .then(veg => {
-      return Gardener.create({
-        name: 'Rick',
-        age: 40,
-        favoriteVegetableId: veg.id,
-      });
+function makePromises() {
+  const vegetablesP = Promise.all([
+    Vegetable.create({ name: 'Carrot', color: 'Orange' }),
+    Vegetable.create({ name: 'Tomato', color: 'Red' }),
+    Vegetable.create({ name: 'Broccoli', color: 'Green' }),
+  ]);
+  const plotsP = vegetablesP
+    .then(([carrot, tomato, broccoli]) => {
+      return Promise.all([
+        Gardener.create({
+          name: 'Bob',
+          age: 35,
+          favoriteVegetableId: carrot.id,
+        }),
+        Gardener.create({
+          name: 'Jordan',
+          age: 25,
+          favoriteVegetableId: tomato.id,
+        }),
+        Gardener.create({
+          name: 'Wendie',
+          age: 50,
+          favoriteVegetableId: broccoli.id,
+        }),
+      ]);
     })
-    .then(gardener => {
-      return Plot.create({
-        size: 400,
-        shaded: true,
-        gardenerId: gardener.id,
-      });
-    })
-    .catch(err => {
-      console.error('thrown in makePromise function', err.stack);
+    .then(([bob, jordan, wendie]) => {
+      return Promise.all([
+        Plot.create({ size: 100, shaded: true, gardenerId: bob.id }),
+        Plot.create({ size: 200, shaded: true, gardenerId: jordan.id }),
+        Plot.create({ size: 500, shaded: false, gardenerId: wendie.id }),
+      ]);
     });
-
-  return Promise.all([vegetableP, plotP]);
+  return Promise.all([vegetablesP, plotsP]);
 }
